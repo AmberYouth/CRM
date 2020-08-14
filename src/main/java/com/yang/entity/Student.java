b838a5dfa4163f88e5706a6fe7c2a3e7fba4b9cd
yangshije <2374575227@qq.com>
2020-08-14 13:52:40 +0800

    update

--- src/main/java/com/yang/entity/Student.java
+++ src/main/java/com/yang/entity/Student.java
@@ -37,20 +37,6 @@ public class Student {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id) &&
-                Objects.equals(name, student.name) &&
-                Objects.equals(age, student.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 
     public void setName(String name) {
         this.name = name;
