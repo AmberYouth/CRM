bfef9cf85e950af0852a7f25bf99d0835ca3c9c5
yangshijie <2374575227@qq.com>
2020-09-08 23:16:04 +0800

    update

--- src/main/java/com/yang/workbench/service/impl/clueServiceImpl.java
+++ src/main/java/com/yang/workbench/service/impl/clueServiceImpl.java
@@ -252,6 +252,7 @@ public class clueServiceImpl implements ClueService {
 //                flag  = false;
 //            }
 //        }
+        System.out.println("");
         for (ClueActivityRelation clueActivityRelation:clueActivityRelationList){
             int count8 = clueActivityRelationDao.delete(clueActivityRelation);
             if (count8!=1){
